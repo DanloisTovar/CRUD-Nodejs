de98ae40e072f4321f61d64b65d2d6fd523ad8d7
Danlois R Tovar T <danlois.tovar@gmail.com>
2020-07-04 16:39:13 -0300

    se realizo la configuracion inicial del servidor (faltan la rutas)

--- src/app.js
+++ src/app.js
@@ -1,9 +1,21 @@
 const express = require('express');
-
+const paht = require('path');
+const morgan = require('morgan');
 const app = express();
 
-app.listen(3000, () => {
-  console.log('====================================');
-  console.log('Server escuchando por el puerto 3000 ');
-  console.log('====================================');
+//Configuracion:
+app.set('port', process.env.PORT || 3000);
+app.set('views', paht.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+//Middelwares:
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: false }));
+
+//Rutas:
+const indexRoutes = require('./routes/routes');
+app.use('/', indexRoutes);
+//Inicio de servidor:
+app.listen(app.get('port'), () => {
+  console.log(`Servidor corriendo por el puerto: ${app.get('port')}!!`);
 });
