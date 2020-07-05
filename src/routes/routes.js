1fd62a54eaead6df8aa5c4c63b20a2a3b6184d81
Danlois R Tovar T <danlois.tovar@gmail.com>
2020-07-05 20:53:49 -0300

    Se praticiono el codigo, se agrego la carpeta partials. se realizo el crud (ingresar, actualizar y borrar datos) de la base de datos y se pinta en la pagina muy divertido. a futuro realizare el bootstrap directo de de la carpeta bootstrap y no de cdn.

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,9 +1,64 @@
 const express = require('express');
 const router = express.Router();
+const Tarea = require('../models/tareas');
 
 //Pagina inicial:
-router.get('/', (req, res) => {
-  res.render('index.ejs');
+
+// GETS:
+router.get('/', async (req, res) => {
+  const tareaTomarInfoBD = await Tarea.find();
+  console.log(tareaTomarInfoBD);
+
+  res.render('index.ejs', {
+    // obtener tareas
+    tareaTomarInfoBD,
+  });
 });
 
+// POSTS:
+router.post('/add', async (req, res) => {
+  const tarea = new Tarea(req.body);
+  await tarea.save();
+  res.redirect('/');
+});
+
+//Prender boton hecho:
+
+router.get('/hecho/:id', async (req, res) => {
+  const { id } = req.params;
+  //buscar id:
+  const tarea = await Tarea.findById(id);
+  //cambiar status:
+  tarea.status = !tarea.status;
+  await tarea.save();
+  console.log(tarea);
+  res.redirect('/');
+});
+
+// PUTS:
+
+//GET
+router.get('/editar/:id', async (req, res) => {
+  const { id } = req.params;
+  const tarea = await Tarea.findById(id);
+  res.render('editar.ejs', {
+    tarea,
+  });
+});
+
+// POST:
+
+router.post('/edit/:id', (req, res) => {
+  const { id } = req.params;
+  Tarea.update({ _id: id }, req.body);
+  res.redirect('/');
+});
+
+// DELETES:
+router.get('/borrar/:id', async (req, res) => {
+  const { id } = req.params;
+  //Eliminar dato de la BD:
+  await Tarea.remove({ _id: id });
+  res.redirect('/');
+});
 module.exports = router;
